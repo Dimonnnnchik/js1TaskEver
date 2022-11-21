e8cd0103f20a96535ced071bb4692cd9ae5896bc
Dimonnnnchik <ananaskorea96@gmail.com>
2022-11-22 00:28:31 +0100

    hz yakui

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
+"use strict"
 
-const numberOfFilms = prompt('How many films have u watched already', '');
+const numberOfFilms = +prompt('How many films have u watched already', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -9,12 +10,31 @@ const personalMovieDB = {
     private: false
 };
 
-const a = prompt('one of recently viewed films', ''),
-    b = prompt('how can u rate it', ''),
-    c = prompt('one of recently viewed films', ''),
-    d = prompt('how can u rate it', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('one of recently viewed films', ''),
+        b = prompt('how can u rate it', '')
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done;')
+    } else {
+        console.log('error');
+        i--;
+    }
 
-console.log(personalMovieDB)
+}
+
+
+if (personalMovieDB.count < 10) {
+    console.log("u've watched a small number of films");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('u r an avarage film watcher');
+} else if (personalMovieDB.count >= 30) {
+    console.log('u r an film enjoyer');
+} else {
+    console.log('error');
+}
+
+
+
+console.log(personalMovieDB);
