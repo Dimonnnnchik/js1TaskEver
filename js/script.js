2a1e2d70a9e4ee849949fbb9b2caf4aa01ad1f8b
Dimonnnnchik <ananaskorea96@gmail.com>
2022-11-21 21:03:07 +0100

    first pract lesson

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log(1)
+
+const numberOfFilms = prompt('How many films have u watched already', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const a = prompt('one of recently viewed films', ''),
+    b = prompt('how can u rate it', ''),
+    c = prompt('one of recently viewed films', ''),
+    d = prompt('how can u rate it', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB)
